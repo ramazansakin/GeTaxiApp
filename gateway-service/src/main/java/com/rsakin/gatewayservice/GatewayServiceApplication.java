723c1049fb9601bd982719546bbded5f33f2a662
Ramazan Sakin <ramazansakin63@gmail.com>
2020-12-27 10:12:13 +0300

    JWT intg on gateway adaptation

--- gateway-service/src/main/java/com/rsakin/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/rsakin/gatewayservice/GatewayServiceApplication.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableFeignClients
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer() {
+        return ServerCodecConfigurer.create();
+    }
 }
